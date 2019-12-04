b1717e02fec898d0d060442ee2ee7bde54fa7350
Kyle Polich <kyle@dataskeptic.com>
2019-12-04 11:37:43 -0800

    persist to dynamodb

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
+import boto3
+
 from chalice import Chalice
 import json
 
 
 app = Chalice(app_name='demo')
-shoutouts = {}
+db = boto3.resource('dynamodb', region_name='us-east-2')
+table = db.Table('ShoutOuts')
+
+data = table.scan()
+print(data)
 
 
 @app.route('/')
 def index():
-    return shoutouts
+    data = table.scan()
+    print(data)
+    return data['Items']
 
 
 @app.route('/shout/{name}', methods=['GET'])
 def get_shout(name):
-    return shoutouts[name]
+    data = table.get_item(Key=name)
+    print(data)
+    return data['Item']
 
 
 @app.route('/shout/{name}', methods=['POST'])
@@ -22,7 +32,9 @@ def shout(name):
     try:
         item = json.loads(shout_out)
     except:
-        item = str(shout_out).strip()
-    shoutouts[name] = item
+        item = {"msg": str(shout_out).strip()}
+    item['name'] = name
+    r = table.put_item(Item=item)
+    print(r)
     return {'ack': True}
 
